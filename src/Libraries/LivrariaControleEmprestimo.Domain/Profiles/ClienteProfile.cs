186bf2a2c3f5b5e3f399c9a8e86b7b2e5302e9d1
Roberwal Junior <roberwal@gpecx.com>
2023-11-14 14:35:31 -0300

    Criando a pasta de profiles na biblioteca de Dominio.

---
+++ src/Libraries/LivrariaControleEmprestimo.Domain/Profiles/ClienteProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using LivrariaControleEmprestimo.Domain.Dtos;
+using LivrariaControleEmprestimo.Domain.Entities;
+
+namespace LivrariaControleEmprestimo.Domain.Profiles;
+
+public class ClienteProfile : Profile
+{
+    public ClienteProfile()
+    {
+        CreateMap<CreateClienteDto, Cliente>();
+        CreateMap<Cliente, ReadClienteDto>();
+        CreateMap<UpdateClienteDto, Cliente>();
+    }
+}
---
+++ src/Libraries/LivrariaControleEmprestimo.Domain/Profiles/LivroClienteEmprestimoProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using LivrariaControleEmprestimo.Domain.Dtos;
+using LivrariaControleEmprestimo.Domain.Entities;
+
+namespace LivrariaControleEmprestimo.Domain.Profiles;
+
+public class LivroClienteEmprestimoProfile : Profile
+{
+    public LivroClienteEmprestimoProfile()
+    {
+        CreateMap<CreateLivroClienteEmprestimoDto, LivroClienteEmprestimo>();
+        CreateMap<LivroClienteEmprestimo, ReadLivroClienteEmprestimoDto>()
+            .ForMember(emprestimoDto => emprestimoDto.Cliente,
+                opt => opt.MapFrom(emprestimo => emprestimo.Cliente))
+            .ForMember(emprestimoDto => emprestimoDto.Livro,
+                opt => opt.MapFrom(emprestimo => emprestimo.Livro));
+        CreateMap<UpdateLivroClienteEmprestimoDto, LivroClienteEmprestimo>();
+    }
+}
