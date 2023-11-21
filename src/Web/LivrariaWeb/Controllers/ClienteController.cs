2bc0d20e43182ed2a7038609acfd6ea2d4a21638
Roberwal Junior <roberwal@gpecx.com>
2023-11-21 10:57:58 -0300

    Criando o projeto Web. Criando as primeiras interfaces do Cliente.

---
+++ src/Web/LivrariaWeb/Controllers/ClienteController.cs
@@ -0,0 +1,39 @@
+﻿using LivrariaWeb.Models;
+using LivrariaWeb.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LivrariaWeb.Controllers;
+
+public class ClienteController : Controller
+{
+    private readonly ClienteApiService _clienteApi;
+
+    public ClienteController(ClienteApiService clienteApi)
+    {
+        _clienteApi = clienteApi;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        List<ClienteViewModel> clientes = await _clienteApi.RecuperarClientes();
+
+        if (clientes == null)
+        {
+            ModelState.AddModelError(null, "Erro ao processar a solicitação");
+        }
+
+        return View(clientes);
+    }
+
+    public async Task<IActionResult> Details(int id)
+    {
+        ClienteViewModel cliente = await _clienteApi.RecuperarClientePeloId(id);
+
+        if (cliente == null)
+        {
+            ModelState.AddModelError(null, "Erro ao processar a solicitação");
+        }
+
+        return View(cliente);
+    }
+}
---
+++ src/Web/LivrariaWeb/Services/ClienteApiService.cs
@@ -0,0 +1,65 @@
+﻿using LivrariaWeb.Models;
+using Newtonsoft.Json;
+
+namespace LivrariaWeb.Services;
+
+public class ClienteApiService
+{
+    private readonly string ENDPOINT;
+    private readonly HttpClient httpClient;
+
+    public ClienteApiService(IConfiguration configuration)
+    {
+        ENDPOINT = configuration["AppConfig:EndPoints:Url_Api"] + "Cliente/";
+        httpClient = new HttpClient
+        {
+            BaseAddress = new Uri(ENDPOINT)
+        };
+    }
+
+    public async Task<List<ClienteViewModel>> RecuperarClientes()
+    {
+        List<ClienteViewModel> clientes = null;
+
+        try
+        {
+            HttpResponseMessage response = await httpClient.GetAsync(ENDPOINT);
+
+            if (response.IsSuccessStatusCode)
+            {
+                string content = await response.Content.ReadAsStringAsync();
+                clientes = JsonConvert.DeserializeObject<List<ClienteViewModel>>(content);
+            }
+
+            return clientes;
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+    }
+
+    public async Task<ClienteViewModel> RecuperarClientePeloId(int id)
+    {
+        try
+        {
+            ClienteViewModel result = null;
+
+            string url = $"{ENDPOINT}{id}";
+
+            HttpResponseMessage response = await httpClient.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                string content = await response.Content.ReadAsStringAsync();
+                result = JsonConvert.DeserializeObject<ClienteViewModel>(content);
+            }
+
+            return result;
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+    }
+}
